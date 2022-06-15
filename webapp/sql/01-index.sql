7212a268d3a5e1c8af9ae2e8db59be25a97de67c
Akihide Ito <39990441+AkkyOrz@users.noreply.github.com>
2022-06-15 21:01:25 +0900

    feat: posts にindexを追加したが、変わらなかった (#5)

--- webapp/sql/01-index.sql
+++ webapp/sql/01-index.sql
@@ -1,2 +1,3 @@
 CREATE INDEX `post_id_created_at_idx` ON `comments` (`post_id`, `created_at` DESC);
 CREATE INDEX `created_at_idx` ON `comments` (`created_at` DESC);
+CREATE INDEX `created_at_idx` ON `posts` (`created_at` DESC);
